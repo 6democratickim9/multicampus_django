f98d43d9ef8dfce3e4da46122f9b6ded633ae535
6democratickim9 <shcomon24@gmail.com>
2021-01-13 11:25:13 +0900

    오늘 배운것

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# Post 목록
+def post_list(request):
+    name = 'Django'
+    response = HttpResponse(content_type ="text/html")
+    response.write(f'<h2>Hello {name}</h2>',)
+    response.write(f'<p>HTTP Method : {request.method}</p>')
+    response.write(f'<p>HTTP ConentType : {request.content_type}</p>')
+    # return HttpResponse(f'''<h2>Hello{name}</h2><p>HTTP METHOD : {request.method}</p>''')
 
-# Create your views here.
+    return response
