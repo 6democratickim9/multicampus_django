f948842f0f9a558574159e479ee1c9127bcb8956
6democratickim9 <shcomon24@gmail.com>
2021-01-13 14:48:49 +0900

    오늘 배운것

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
 #     작성일
     created_date = models.DateTimeField(default=timezone.now)
 #     게시일--> 값이 비어있어도 ㄱㅊ
-    publshed_date = models.DateTimeField(blank=True,null=True)
+    published_date = models.DateTimeField(blank=True,null=True)
 
     #  migration test--> post에 test 변수를 만들어줬기 떄문에 0002post_test가 된 것
     # test = models.TextField()
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from .models import Post
 # Post 목록
-def post_list(request):
+def post_response(request):
     name = 'Django'
-    response = HttpResponse(content_type ="text/html")
-    response.write(f'<h2>Hello {name}</h2>',)
+    response = HttpResponse(f'<h2>Hello {name}</h2>',content_type ="text/html")
+    response.write(f'<h2>Hello {name}</h2>')
     response.write(f'<p>HTTP Method : {request.method}</p>')
     response.write(f'<p>HTTP ConentType : {request.content_type}</p>')
     # return HttpResponse(f'''<h2>Hello{name}</h2><p>HTTP METHOD : {request.method}</p>''')
 
     return response
+
+# Post 목록
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).\
+        order_by('published_date')
+    # 현재날짜보다 작거나 같은 값을 가져오고 어센딩 해라
+    return render(request,'blog/post_list.html',{'posts': posts})
+    # 템플릿을 연결해주는 역할
