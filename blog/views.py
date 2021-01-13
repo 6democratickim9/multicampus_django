70835403575e2ba66105e4aab1443b37a5af60a0
6democratickim9 <shcomon24@gmail.com>
2021-01-13 17:20:49 +0900

    오늘 배운것

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.utils import timezone
+from requests import post
+
 from .models import Post
 
 
@@ -8,18 +10,19 @@ from .models import Post
 # post 상세정보
 
 def post_detail(request,pk):
-    get_object_or_404(Post,pk=pk)
-    return render(request'blog/post_detail.html',{'post': post})
+    post = get_object_or_404(Post, pk=pk)
+    return render(request,'blog/post_detail.html',{'post': post})
 
 
 
 # Post 목록
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).\
-        order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
     # 현재날짜보다 작거나 같은 값을 가져오고 어센딩 해라
-    return render(request,'blog/post_list.html',{'posts': posts})
+
     # 템플릿을 연결해주는 역할
 
 
